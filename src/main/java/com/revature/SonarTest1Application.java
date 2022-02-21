b86af642d4c2ef1a82c708ef5ecc6140857cae17
Simple Dimple John <simpledimplejohn@gmail.com>
2022-02-21 14:16:51 -0800

    initial commit java 11

---
+++ src/main/java/com/revature/SonarTest1Application.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarTest1Application.class, args);
+	}
+
+}
